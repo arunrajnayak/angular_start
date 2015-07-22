032933bded671aa9be840d1f45063abf8b69f64b
Arunraj Nayak <arunrajnayak1@gmail.com>
2015-07-22 15:11:39 +0530

    v0.7 details route prep

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
-var myApp=angular.module('myApp',['ngRoute','artistControllers']);
+var myApp = angular.module('myApp', [
+  'ngRoute',
+  'artistControllers'
+]);
 
-myApp.config(['$routeProvider',function($routeProvider){
-	$routeProvider.
-	when('/list', {
-		templateUrl:'partials/list.html',
-		controller: 'ListController'
-	}).
-	otherwise({
-		redirectTo:'/List'
-	});
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+  when('/list', {
+    templateUrl: 'partials/list.html',
+    controller: 'ListController'
+  }).
+  when('/details/:itemId', {
+    templateUrl: 'partials/details.html',
+    controller: 'DetailsController'
+  }).
+  otherwise({
+    redirectTo: '/list'
+  });
 }]);
